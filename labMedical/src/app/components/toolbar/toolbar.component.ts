c9eb68997104fd3e46511dddd2103be1a3920335
Osnei Luzia <osnei.luzia@gmail.com>
2023-01-24 22:44:51 -0300

    Mudança Dinâmica de Nome de Página na ToolBar

--- labMedical/src/app/components/toolbar/toolbar.component.ts
+++ labMedical/src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResolveEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'labM-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit{
+  nomePagina: String | undefined
 
+  ngOnInit(): void {
+  this.setNomePagina(this.rota.url)
+  }  
+  
+  setNomePagina(url:any): void {
+    switch (url.split("/")[2]) {
+      case "inicio":
+        this.nomePagina = "Estatísticas"
+        break;
+      case "paciente":
+        this.nomePagina = "Cadastro de Pacientes"
+        break;
+      case "consulta":
+        this.nomePagina = "Cadastro de Consultas"
+        break;
+      case "exame":
+        this.nomePagina = "Cadastro de Exames"
+        break;
+      case "prontuarios":
+        this.nomePagina = "Prontuário"
+        break;
+    }
+  }
+  constructor(private rota: Router) {
+    rota.events.subscribe((event)=>{
+      if(event instanceof ResolveEnd){
+        this.setNomePagina(event.url)
+      }
+    })
+  }
 }
+
+// /home/paciente/52d65164-fa64-46e6-ab05-9e13f4c7c64e
